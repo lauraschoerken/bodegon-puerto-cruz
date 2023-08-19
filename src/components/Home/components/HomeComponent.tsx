4a281dcf04e04525de1adf300b0140e15771c130
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-19 19:34:24 +0100

    feat: add home-welcome

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -9,22 +9,17 @@ const HomeComponent = () => {
 				<Header />
 			</div>
 			<div className='body'>
-				<p>Cuerpo</p>
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
-				<p>Cuerpo</p> <p>Cuerpo</p>
+				<div className='welcome'>
+					<div>
+						{' '}
+						<h2>Bienvenido al Bodegón Puerto de la Cruz</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+							dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+							nascetur ridiculus mus.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	)

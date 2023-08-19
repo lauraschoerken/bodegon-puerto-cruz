16ca4b14da4c7a4de3569fc42e997ff02a746722
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-19 18:53:15 +0100

    feat: add home

---
+++ src/components/Home/components/HomeComponent.tsx
@@ -0,0 +1,33 @@
+import Header from '../../Header/containers/Header'
+import './HomeComponent.scss'
+import React from 'react'
+
+const HomeComponent = () => {
+	return (
+		<div className='main'>
+			<div className='header'>
+				<Header />
+			</div>
+			<div className='body'>
+				<p>Cuerpo</p>
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p> <p>Cuerpo</p>{' '}
+				<p>Cuerpo</p> <p>Cuerpo</p>
+			</div>
+		</div>
+	)
+}
+
+export default HomeComponent

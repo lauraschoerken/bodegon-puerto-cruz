9142b2e59d7f7e644190049b993f9f6ebcea77ca
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-23 17:54:14 +0100

    refactor: HomeComponent
    Co-authored-by: David Hormiga Afonso <davidhormigafonso@gmail.com>

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -1,4 +1,6 @@
 import Header from '../../Header/containers/Header'
+import MenuElement from '../../elements/MenuElement/MenuElement'
+import SectionBanner from '../../elements/SectionBanner/SectionBanner'
 import './HomeComponent.scss'
 import React from 'react'
 
@@ -9,17 +11,14 @@ const HomeComponent = () => {
 				<Header />
 			</div>
 			<div className='body'>
-				<div className='welcome'>
-					<div>
-						{' '}
-						<h2>Bienvenido al Bodegón Puerto de la Cruz</h2>
-						<p>
-							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-							dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-							nascetur ridiculus mus.
-						</p>
-					</div>
-				</div>
+				<SectionBanner
+					title='Bienvenido al Bodegón Puerto de la Cruz'
+					body='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+					SideComponent={<>ㅤ</>}
+					justifyContent='space-around'
+					backgroundImage='https://s3-alpha-sig.figma.com/img/adf0/dbff/a4c0afb21f7b08e8a81a363f60fdfe25?Expires=1693180800&Signature=lVadcBYplTmlQZueo-HogbiYW-EZgOwLOU5IdhyIzUW-EDTwaMqvfu0S5v7yK1m4jagwWErb0p5Yz066C2aDn8V-BjgztTSKIVZOTzM0x5~HspdWEJYMpzxXS~RwpV~AP3466ANHB0wHfqEcyEd0q4NqK4ebQtKF2nZGUAHqUh2a7Ae6GAWLM-OD~f2SY4P0yAQ5cAicIUJEBHZD~AECawpY8URdfgbG2cERz0JfrDAQrTeqXJJl46PCkWIKkcr7w6ydwhgEaSHj2wPvlB3wZM9m~YVvf6nCmo9yHpAiVAwsjt312vsAnwDvKGAZVLqNrcyB73PNfBM4V9252F14VQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+				/>
+				<MenuElement />
 			</div>
 		</div>
 	)

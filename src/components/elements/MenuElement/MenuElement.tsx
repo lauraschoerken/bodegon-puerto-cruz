84ea4babdb59c3866aaec24435b49b64435f2408
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-23 17:55:08 +0100

    feat: MenuElement component
    Co-authored-by: David Hormiga Afonso <davidhormigafonso@gmail.com>

---
+++ src/components/elements/MenuElement/MenuElement.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './MenuElement.scss'
+import SectionBanner from '../SectionBanner/SectionBanner'
+import { secondary_background } from '../../../utilities/colors'
+
+const MenuElement = () => {
+	const navigateToMenu = () => {
+		console.log('navigate to menu')
+	}
+	return (
+		<SectionBanner
+			title='Mira nuestro menú'
+			body='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+			buttonText='Ver menú'
+			buttonClick={navigateToMenu}
+			SideComponent={
+				<div className='menu-element-images'>
+					<img
+						src='https://s3-alpha-sig.figma.com/img/8976/6e6a/2291e97a8b5c5afaf3f292bb6732bc9d?Expires=1693180800&Signature=P9IaXV6GEuBeU7bY5fC5uj7RbyIMIngjR3pRGcfg9MBZk0YgjWbEh2lDfdY3oObwJ~u~vxdH-Zgsi3kB6ANfHsc6DfDwwroys24zaAeCwgH5sJWEm1COQ8lPpu5LMYo5poaPOWq3IiDFMTOFykXCuUFWbTBL4tpMzdRt0-4ipxvcgTE9m~muEa8zfrkxxEHUFiHOUU1ZKqVIVL6pcN7RP2AxHhZkj12J765EwJyTAZ1MyOR504MuM7aou8thhxecvZQ3oARLBV1fzpeSOjB7Rer70nn8sIsfOVRS7y9G3vc17bETYiwfdd0ESPbgXlWZXw7eITUlZjt9NFgY8wumPw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+						alt=''
+					/>
+					<img
+						src='https://s3-alpha-sig.figma.com/img/8976/6e6a/2291e97a8b5c5afaf3f292bb6732bc9d?Expires=1693180800&Signature=P9IaXV6GEuBeU7bY5fC5uj7RbyIMIngjR3pRGcfg9MBZk0YgjWbEh2lDfdY3oObwJ~u~vxdH-Zgsi3kB6ANfHsc6DfDwwroys24zaAeCwgH5sJWEm1COQ8lPpu5LMYo5poaPOWq3IiDFMTOFykXCuUFWbTBL4tpMzdRt0-4ipxvcgTE9m~muEa8zfrkxxEHUFiHOUU1ZKqVIVL6pcN7RP2AxHhZkj12J765EwJyTAZ1MyOR504MuM7aou8thhxecvZQ3oARLBV1fzpeSOjB7Rer70nn8sIsfOVRS7y9G3vc17bETYiwfdd0ESPbgXlWZXw7eITUlZjt9NFgY8wumPw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+						alt=''
+					/>
+				</div>
+			}
+			justifyContent='space-around'
+			backgroundColor={secondary_background}
+		/>
+	)
+}
+
+export default MenuElement

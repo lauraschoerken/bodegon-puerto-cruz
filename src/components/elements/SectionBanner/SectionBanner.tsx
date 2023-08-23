67a880bddc45259dfaecff59e2f3b97054ad1347
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-23 17:53:16 +0100

    feat: SectionBanner component
    Co-authored-by: David Hormiga Afonso <davidhormigafonso@gmail.com>

---
+++ src/components/elements/SectionBanner/SectionBanner.tsx
@@ -0,0 +1,45 @@
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react'
+import TextBlock from '../TextBlock/TextBlock'
+import './SectionBanner.scss'
+
+interface Props {
+	title: string
+	body?: string
+	buttonText?: string
+	buttonClick?: () => void
+	backgroundColor?: string
+	backgroundImage?: string
+	justifyContent?: 'space-between' | 'space-around' | 'space-evenly' | 'center'
+	SideComponent?: React.ReactNode
+}
+
+const SectionBanner: React.FC<Props> = (props) => {
+	const {
+		title,
+		body,
+		buttonText,
+		buttonClick,
+		backgroundColor,
+		backgroundImage,
+		justifyContent = 'space-around',
+		SideComponent,
+	} = props
+
+	const wrapperStyles = () => {
+		let styles: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> = {}
+		if (backgroundColor) styles['backgroundColor'] = backgroundColor
+		if (backgroundImage) styles['backgroundImage'] = `url(${backgroundImage})`
+		if (justifyContent) styles['justifyContent'] = justifyContent
+
+		return styles
+	}
+
+	return (
+		<div className='section-banner-wrapper' style={wrapperStyles()}>
+			<TextBlock title={title} body={body} buttonText={buttonText} buttonClick={buttonClick} />
+			{SideComponent && SideComponent}
+		</div>
+	)
+}
+
+export default SectionBanner

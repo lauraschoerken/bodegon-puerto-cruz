efde62cc8351c4e17d6d77cdcb1841d34f1196bc
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-26 13:11:39 +0100

    lang: add i18n home

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import Header from '../../Header/containers/Header'
 import MenuElement from '../../elements/MenuElement/MenuElement'
 import SectionBanner from '../../elements/SectionBanner/SectionBanner'
@@ -5,6 +6,8 @@ import './HomeComponent.scss'
 import React from 'react'
 
 const HomeComponent = () => {
+	const { t } = useTranslation()
+
 	return (
 		<div className='main'>
 			<div className='header'>
@@ -12,8 +15,8 @@ const HomeComponent = () => {
 			</div>
 			<div className='body'>
 				<SectionBanner
-					title='Bienvenido al Bodegón Puerto de la Cruz'
-					body='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+					title={t('home.title')}
+					body={t('home.description')}
 					SideComponent={<>ㅤ</>}
 					justifyContent='space-around'
 					backgroundImage='https://s3-alpha-sig.figma.com/img/adf0/dbff/a4c0afb21f7b08e8a81a363f60fdfe25?Expires=1693180800&Signature=lVadcBYplTmlQZueo-HogbiYW-EZgOwLOU5IdhyIzUW-EDTwaMqvfu0S5v7yK1m4jagwWErb0p5Yz066C2aDn8V-BjgztTSKIVZOTzM0x5~HspdWEJYMpzxXS~RwpV~AP3466ANHB0wHfqEcyEd0q4NqK4ebQtKF2nZGUAHqUh2a7Ae6GAWLM-OD~f2SY4P0yAQ5cAicIUJEBHZD~AECawpY8URdfgbG2cERz0JfrDAQrTeqXJJl46PCkWIKkcr7w6ydwhgEaSHj2wPvlB3wZM9m~YVvf6nCmo9yHpAiVAwsjt312vsAnwDvKGAZVLqNrcyB73PNfBM4V9252F14VQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'

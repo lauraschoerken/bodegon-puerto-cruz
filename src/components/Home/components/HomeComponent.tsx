8f1d08b18f8edff53c4972c46d707b1a5d2204d9
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-26 14:13:47 +0100

    feat: add contact and location to Home

--- src/components/Home/components/HomeComponent.tsx
+++ src/components/Home/components/HomeComponent.tsx
@@ -4,6 +4,8 @@ import MenuElement from '../../elements/MenuElement/MenuElement'
 import SectionBanner from '../../elements/SectionBanner/SectionBanner'
 import './HomeComponent.scss'
 import React from 'react'
+import LocationElement from '../../elements/LocationElement/LocationElement'
+import ContactElement from '../../elements/ContactElement/ContactElement'
 
 const HomeComponent = () => {
 	const { t } = useTranslation()
@@ -22,6 +24,8 @@ const HomeComponent = () => {
 					backgroundImage='https://s3-alpha-sig.figma.com/img/adf0/dbff/a4c0afb21f7b08e8a81a363f60fdfe25?Expires=1693180800&Signature=lVadcBYplTmlQZueo-HogbiYW-EZgOwLOU5IdhyIzUW-EDTwaMqvfu0S5v7yK1m4jagwWErb0p5Yz066C2aDn8V-BjgztTSKIVZOTzM0x5~HspdWEJYMpzxXS~RwpV~AP3466ANHB0wHfqEcyEd0q4NqK4ebQtKF2nZGUAHqUh2a7Ae6GAWLM-OD~f2SY4P0yAQ5cAicIUJEBHZD~AECawpY8URdfgbG2cERz0JfrDAQrTeqXJJl46PCkWIKkcr7w6ydwhgEaSHj2wPvlB3wZM9m~YVvf6nCmo9yHpAiVAwsjt312vsAnwDvKGAZVLqNrcyB73PNfBM4V9252F14VQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
 				/>
 				<MenuElement />
+				<LocationElement />
+				<ContactElement />
 			</div>
 		</div>
 	)

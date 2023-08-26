e862d25c3f0d6a65c219c5350a9a83b35ad9eb63
Laura Schoerken <Lauraschorken@gmail.com>
2023-08-26 13:11:20 +0100

    lang: add i18n header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from './components/Home/containers/Home'
 import './App.scss'
+import i18n from './i18n'
+import { I18nextProvider } from 'react-i18next'
 
 function App() {
 	return (
-		<>
+		<I18nextProvider i18n={i18n}>
 			<Home />
-		</>
+		</I18nextProvider>
 	)
 }
 
